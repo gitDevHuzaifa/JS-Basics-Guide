79e0e1fa58db25707eafd9b46be0ca40dcd0b464
Abu Huzaifa <168542627+gitDevHuzaifa@users.noreply.github.com>
2025-01-20 23:46:08 +0530

    Create 03_objects.js

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,15 @@
+
+// Singleton
+// boject literals
+
+// Object.create
+const jsUser = {
+    name : "Huzafia",
+    age : 18,
+    location : "New Delhi",
+    email: "huzaifa@google.com",
+    isLoggedIn : false,
+    lastLoginDays : ["Monday", "Saturday"]
+}
+console.log(jsUser.email);
+console.log(jsUser["email"])
