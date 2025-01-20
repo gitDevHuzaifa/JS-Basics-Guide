aa3318aa614e390c2dcd9d58e6069cefecc6c9e8
Abu Huzaifa <168542627+gitDevHuzaifa@users.noreply.github.com>
2025-01-20 12:54:00 +0530

    Update 02_arrays.js

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -10,11 +10,18 @@ const dcHeros = ["batman", "flash", "superman"];
 // const all_heros = marvelHeros.concat(dcHeros);
 // console.log(all_heros)
 
-// const all_new_heros = [...marvelHeros, ...dcHeros]  // !Improtant for concatinations
+const all_new_heros = [...marvelHeros, ...dcHeros]  // !Improtant for concatinations
 // console.log(all_new_heros)
 
 const another_array = [ 1,2,3,4,5,[6,7,8,9,[1,2,3,]]]
 const real_another_array = another_array.flat(Infinity);
 console.log(real_another_array)
+
+console.log(all_new_heros[5]);
+// console.log("hii0");
+
+
+
+
 
 
